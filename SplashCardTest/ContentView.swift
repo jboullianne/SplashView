baf010ff5eaa876a5da0f8b174a6581536ee4df1
Jean-Marc Boullianne <jboullianne@gmail.com>
2020-04-24 18:49:09 -0500

    Initial Commit

---
+++ SplashCardTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SplashCardTest
+//
+//  Created by Jean-Marc Boullianne on 4/24/20.
+//  Copyright © 2020 TrailingClosure. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
